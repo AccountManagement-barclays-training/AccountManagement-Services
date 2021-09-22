61a61683c9ecb2352718e8148979e4aa3dc6887b
root <root@DESKTOP-8RLO52P.localdomain>
2021-09-22 21:04:12 +0530

    Added thymleaf dependency

--- src/main/java/com/accountmanagement/Account_Management/AccountManagementApplication.java
+++ src/main/java/com/accountmanagement/Account_Management/AccountManagementApplication.java
@@ -1,14 +1,29 @@
 package com.accountmanagement.Account_Management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 
 @SpringBootApplication
 public class AccountManagementApplication {
-
+	@Autowired
+	private JavaMailSender javamailsender;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AccountManagementApplication.class, args);
+	}
+	
+	public void sendEmail()
+	{
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo("sharrikvk18@gmail.com");
+		msg.setSubject("Test Mail");
+		msg.setText("Testing..");
+		javamailsender.send(msg);
+
 	}
 
 }
